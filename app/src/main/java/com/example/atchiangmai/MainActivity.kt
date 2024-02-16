90b90c89ccfeed921d4153fc032b00ffe7989f52
longjong <117634233+longjong@users.noreply.github.com>
2024-02-17 02:19:25 +0700

    worachon slide and item-menu

--- app/src/main/java/com/example/atchiangmai/MainActivity.kt
+++ app/src/main/java/com/example/atchiangmai/MainActivity.kt
@@ -1,25 +1,134 @@
 package com.example.atchiangmai
 
+import com.example.atchiangmai.adapters.ImageAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.denzcoskun.imageslider.ImageSlider
-import com.denzcoskun.imageslider.constants.ScaleTypes
-import com.denzcoskun.imageslider.models.SlideModel
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.viewpager2.widget.ViewPager2
+import com.example.atchiangmai.models.ImageItem
+import java.util.UUID
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewpager2 : ViewPager2
+    private lateinit var pageChangeListener: ViewPager2.OnPageChangeCallback
+
+    private val params = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    ).apply {
+        setMargins(8,0,8,0)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageSlider = findViewById<ImageSlider>(R.id.imageSlide)
-        val imageList = ArrayList<SlideModel>()
 
-        imageList.add(SlideModel("https://images.immediate.co.uk/production/volatile/sites/3/2019/04/Avengers-Endgame-Banner-2-de7cf60.jpg?quality=90&resize=620,413","Avengers Endgame"))
-        imageList.add(SlideModel("https://img.cinemablend.com/filter:scale/quill/3/7/0/0/8/e/37008e36e98cd75101cf1347396eac8534871a19.jpg?mw=600","Jumanji"))
-        imageList.add(SlideModel("https://www.adgully.com/img/800/201711/spider-man-homecoming-banner.jpg","Spider Man"))
-        imageList.add(SlideModel("https://live.staticflickr.com/1980/29996141587_7886795726_b.jpg","Venom"))
+         viewpager2 = findViewById<ViewPager2>(R.id.viewpager2)
+        val slideDotll = findViewById<LinearLayout>(R.id.slideDotLL)
 
-        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        val imageList = arrayListOf(
+            ImageItem(
+
+                //ใช้แบบดึงรูปมาจากโฟลเดอร์drawable
+
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.cheangdao
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.intanon
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.jdluang
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.kunluang
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.maewang
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.mondjam
+            )
+        )
+
+        //ใช้แบบดึงรูปมาจากเว็บไซต์
+
+//        val imageList = arrayListOf(
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/866/500/500.jpg?hmac=FOptChXpmOmfR5SpiL2pp74Yadf1T_bRhBF1wJZa9hg"
+//            ),
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/270/500/500.jpg?hmac=MK7XNrBrZ73QsthvGaAkiNoTl65ZDlUhEO-6fnd-ZnY"
+//            ),
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/320/500/500.jpg?hmac=2iE7TIF9kIqQOHrIUPOJx2wP1CJewQIZBeMLIRrm74s"
+//            ),
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/798/500/500.jpg?hmac=Bmzk6g3m8sUiEVHfJWBscr2DUg8Vd2QhN7igHBXLLfo"
+//            ),
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/95/500/500.jpg?hmac=0aldBQ7cQN5D_qyamlSP5j51o-Og4gRxSq4AYvnKk2U"
+//            ),
+//            ImageItem(
+//                UUID.randomUUID().toString(),
+//                "https://fastly.picsum.photos/id/778/500/500.jpg?hmac=jZLZ6WV_OGRxAIIYPk7vGRabcAGAILzxVxhqSH9uLas"
+//            )
+//        )
+
+
+        val imageAdapter = ImageAdapter()
+        viewpager2.adapter = imageAdapter
+        imageAdapter.submitList(imageList)
+
+        val slideDotLL = findViewById<LinearLayout>(R.id.slideDotLL)
+        val dotsImage = Array(imageList.size) { ImageView(this) }
+
+        dotsImage.forEach {
+            it.setImageResource(
+                R.drawable.non_active_dot
+            )
+            slideDotLL.addView(it,params)
+        }
+
+        // default first dot selected
+        dotsImage[0].setImageResource(R.drawable.active_dot)
+
+        pageChangeListener = object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                dotsImage.mapIndexed { index, imageView ->
+                    if (position == index){
+                        imageView.setImageResource(
+                            R.drawable.active_dot
+                        )
+                    }else{
+                        imageView.setImageResource(R.drawable.non_active_dot)
+                    }
+                }
+                super.onPageSelected(position)
+            }
+        }
+        viewpager2.registerOnPageChangeCallback(pageChangeListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewpager2.unregisterOnPageChangeCallback(pageChangeListener)
     }
 }
+
+
---
+++ app/src/main/java/com/example/atchiangmai/models/ImageItem.kt
@@ -0,0 +1,6 @@
+package com.example.atchiangmai.models
+
+data class ImageItem(
+    val id : String,
+    val url : String
+)
